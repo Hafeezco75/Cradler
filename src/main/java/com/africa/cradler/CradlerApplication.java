06f61980062d9911461c21d4cc4657762098b1df
Odunaye Abdulhafeez <51507348+Hafeezco75@users.noreply.github.com>
2024-12-03 04:00:53 +0100

    Add files via upload

---
+++ src/main/java/com/africa/cradler/CradlerApplication.java
@@ -0,0 +1,13 @@
+package com.africa.cradler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CradlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CradlerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/africa/cradler/CradlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.africa.cradler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CradlerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
